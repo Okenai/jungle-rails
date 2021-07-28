f1080ddf2520f64f6a9269cdcb1de6c786e1ee8f
Lana <kassembayeva@gmail.com>
2021-07-28 15:50:29 +0000

    active mailer was practiced and error handling at signup was practiced

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,21 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
+
+    if user.valid?
+      user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      redirect_to signup_path, flash: { error: user.errors.full_messages}
     end
+
+    # if user.save
+    #   session[:user_id] = user.id
+    #   redirect_to '/'
+    # else
+    #   redirect_to '/signup'
+    # end
   end  
   
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,8 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
- 
+  
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
